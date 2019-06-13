ad00b3d7f6576d3a58ae49f72f6ee5a963eba08c
jj280522 <jj280522@gmail.com>
2019-06-14 07:12:00 +0800

    update final.js

--- final/fianl.js
+++ final/fianl.js
@@ -15,31 +15,22 @@ function buttonC(){
 function buttonD(){
 			document.getElementById("box4").value = "";
 			document.getElementById("box5").value = "";
-			document.getElementById("box6").value = "";
-        }
-function buttonE()
-		{
+			document.getElementById("box6").value = "";}
+function buttonE(){
 			x = document.getElementById("box7").value;
 			y = document.getElementById("box8").value;
             var z = parseFloat(x) * parseFloat(y);
-            document.getElementById("box9").value = parseFloat(z);
-		}
-function buttonF()
-		{
+            document.getElementById("box9").value = parseFloat(z);}
+function buttonF(){
 			document.getElementById("box7").value = "";
 			document.getElementById("box8").value = "";
-			document.getElementById("box9").value = "";
-        }
-function buttonG()
-		{
+			document.getElementById("box9").value = "";}
+function buttonG(){
 			x = document.getElementById("box10").value;
 			y = document.getElementById("box11").value;
             var z = parseFloat(x) / parseFloat(y);
-            document.getElementById("box12").value = parseFloat(z);
-		}
-function buttonH()
-		{
+            document.getElementById("box12").value = parseFloat(z);}
+function buttonH(){
 			document.getElementById("box10").value = "";
 			document.getElementById("box11").value = "";
-			document.getElementById("box12").value = "";
-		}
+			document.getElementById("box12").value = "";}
